eae8b1f67bfe13d228ac52583a3c830f6d111b58
Gustavo S. Ferreyro <SE33757@grupo.ypf.com>
2021-09-22 17:22:58 -0300

    Add utils.psm1

---
+++ utils.psm1
@@ -0,0 +1,14 @@
+<#
+		.SYNOPSIS
+		Useful functions
+
+		.DESCRIPTION
+		Set of commonly used useful functions
+	
+		.NOTES
+		Author: Gustavo S. Ferreyro, gsferreyro@gmail.com
+        Creation date: 22 September 2021
+
+		This script is provided "AS IS" with no warranty expressed or implied. Run at your own risk.
+		This work is licensed under a Creative Commons 1.0 Universal. See LICENCE file
+#>
