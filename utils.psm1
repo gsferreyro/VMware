bc92c469ee16a8e036d861e3173bd79e953451d7
Gustavo S. Ferreyro <SE33757@grupo.ypf.com>
2021-10-22 00:25:46 -0300

    Update utils.psm1

--- utils.psm1
+++ utils.psm1
@@ -12,7 +12,27 @@
 		This script is provided "AS IS" with no warranty expressed or implied. Run at your own risk.
 		This work is licensed under a Creative Commons 1.0 Universal. See LICENCE file
 #>
+function Test-PowerCLI {
+    if (Get-InstalledModule | Where-Object {$_.Name -eq "VMware.PowerCLI"}) {
+        $bReturn = $true
+    } else {
+        $bReturn = $false
+    }
+    return $bReturn
+}
 
+function now {
+    return $(Get-Date -Format "dd/MM/yyyy HH:mm:ss")
+}
+function Get-Info {
+    return "[INFO] [$(now)]"
+}
+function Get-Warn {
+    return "[WARN] [$(now)]"
+}
+function Get-Error {
+    return "[ERROR] [$(now)]"
+}
 function Write-Tee {
     <#
             .SYNOPSIS
@@ -40,3 +60,10 @@ function Write-Tee {
     Write-Host $InputObject
     if ($FilePath) { Out-File @htParams }
 }
+
+Export-ModuleMember -Function Test-PowerCLI
+Export-ModuleMember -Function now
+Export-ModuleMember -Function Get-Info
+Export-ModuleMember -Function Get-Warn
+Export-ModuleMember -Function Get-Error
+Export-ModuleMember -Function Write-Tee
